8426538e9550376c6c7df3595e37ca55cd09b447
Jacob Laframboise <jlaframboise@cs.queensu.ca>
2019-04-21 12:09:07 -0400

    Now there are gui elements and corresponding functionality to view the passwords in an Accordion, to delete the currently expanded password from the accordion and the vault, and a button to clear all passwords currently stored in the vault.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,14 +2,15 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 
@@ -17,43 +18,69 @@ public class Main extends Application {
 
     Stage window;
     Button addPass;
+    public Accordion passwordViewer;
+    public Vault vault;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         window = primaryStage;
         window.setTitle("JPass - Home");
-        window.setMinWidth(300);
-        window.setMinHeight(500);
+        //window.setMinWidth(300);
+        //window.setMinHeight(500);
 
-        Vault vault = new Vault();
+        vault = new Vault();
 
 
         Button addPassButton = new Button("Add Password");
         addPassButton.setOnAction(e-> {
             vault.addPassword(PasswordAddForm.display());
+            updatePasswordViewer();
         });
 
-        Button viewVaultButton = new Button("View Passwords");
-        viewVaultButton.setOnAction(e->{
-            for (Password p : vault.getPasswords()){
-                System.out.println(p.toString());
-            }
-        });
+        Button removePassButton = new Button("Remove Selected Password");
+        removePassButton.setOnAction(e->deleteSelectedPassword());
 
+        Button clearVaultButton = new Button("Clear Vault");
+        clearVaultButton.setOnAction(e->clearPasswords());
 
         Label vaultPathLabel = new Label("File path for vault");
         TextField vaultPathField = new TextField("Vault Path");
+        Label vaultShiftLabel = new Label("Encryption key (#)");
+        TextField vaultShiftField = new TextField("1");
         Button saveVaultButton = new Button("Save vault");
-        saveVaultButton.setOnAction(e->vault.saveVault(vaultPathField.getText(), 2));
+        saveVaultButton.setOnAction(e->vault.saveVault(vaultPathField.getText(), Integer.parseInt(vaultShiftField.getText())));
 
         Button loadVaultButton = new Button("Load Vault");
-        loadVaultButton.setOnAction(e-> vault.loadVault(vaultPathField.getText(), 2));
+        loadVaultButton.setOnAction(e-> {
+            vault.loadVault(vaultPathField.getText(), Integer.parseInt(vaultShiftField.getText()));
+            updatePasswordViewer();
+        });
 
-        VBox layout = new VBox(10);
-        layout.getChildren().addAll(addPassButton, viewVaultButton, vaultPathLabel, vaultPathField, saveVaultButton, loadVaultButton);
-        layout.setAlignment(Pos.CENTER);
-        Scene scene = new Scene(layout);
+        VBox vaultOptionsLayout = new VBox(10);
+        vaultOptionsLayout.getChildren().addAll(vaultPathLabel, vaultPathField, vaultShiftLabel, vaultShiftField, saveVaultButton, loadVaultButton);
+        vaultOptionsLayout.setAlignment(Pos.CENTER);
+        vaultOptionsLayout.setPadding(new Insets(10,10,10,10));
+
+        VBox centerLayout = new VBox(10);
+        centerLayout.getChildren().addAll(addPassButton, removePassButton, clearVaultButton);
+        centerLayout.setAlignment(Pos.CENTER);
+        centerLayout.setPadding(new Insets(10,10,10,10));
+
+        passwordViewer = new Accordion();
+
+        VBox passViewerLayout = new VBox(10);
+        passViewerLayout.getChildren().addAll(passwordViewer);
+        passViewerLayout.setAlignment(Pos.CENTER);
+        passViewerLayout.setPadding(new Insets(10,10,10,10));
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.setLeft(vaultOptionsLayout);
+        borderPane.setCenter(centerLayout);
+        borderPane.setRight(passViewerLayout);
+        borderPane.setPrefWidth(400);
+        borderPane.setPrefHeight(300);
+        Scene scene = new Scene(borderPane);
         window.setScene(scene);
         window.show();
         //primaryStage.setScene(new Scene(root, 300, 275));
@@ -67,5 +94,30 @@ public class Main extends Application {
 //        System.out.println("Decrypting");
 //        System.out.println(Encryption.decrypt(" ", 1));
         launch(args);
+    }
+
+    public void updatePasswordViewer(){
+        passwordViewer.getPanes().removeAll(passwordViewer.getPanes());
+        for (Password p : vault.getPasswords()){
+            TitledPane tp = new TitledPane(p.getSiteTitle(), new Text(p.toString()));
+            passwordViewer.getPanes().add(tp);
+        }
+        //passwordViewer.getPanes().addAll();
+    }
+
+    public void deleteSelectedPassword(){
+        System.out.println(passwordViewer.getExpandedPane().getText());
+        for (int i = 0; i< vault.getPasswords().size();i++){
+            if (vault.getPasswords().get(i).getSiteTitle().equals(passwordViewer.getExpandedPane().getText())){
+                vault.getPasswords().remove(vault.getPasswords().get(i));
+                break;
+            }
+        }
+        updatePasswordViewer();
+    }
+
+    public void clearPasswords(){
+        vault.getPasswords().clear();
+        updatePasswordViewer();
     }
 }
